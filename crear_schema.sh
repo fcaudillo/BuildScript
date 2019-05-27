924fc79dc905226ab9bc4d45d27ef1b3aa2a0968
Francisco Caudillo <fcaudilloperez1971@gmail.com>
2019-05-27 15:21:13 -0500

    Correcion scrip

--- crear_schema.sh
+++ crear_schema.sh
@@ -1,6 +1,6 @@
-CREATE SCHEMA $1 AUTHORIZATION adm_$1;
-ALTER USER adm_$1 SET search_path = $1, public;
-ALTER USER $1 WITH PASSWORD '$2';
-GRANT CONNECT ON DATABASE dbtpv TO adm_$1;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA $1 TO adm_$1;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA $1 TO adm_$1;
+echo "CREATE SCHEMA $1 AUTHORIZATION adm_$1;" > ./scripts/create_schema_$1.sql
+echo "ALTER USER adm_$1 SET search_path = $1, public;" >> ./scripts/create_schema_$1.sql
+echo "ALTER USER $1 WITH PASSWORD '$2';" >> ./scripts/create_schema_$1.sql
+echo "GRANT CONNECT ON DATABASE dbtpv TO adm_$1;" >> ./scripts/create_schema_$1.sql
+echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA $1 TO adm_$1;" >> ./scripts/create_schema_$1.sql
+echo "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA $1 TO adm_$1;" >> ./scripts/create_schema_$1.sql
