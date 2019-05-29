b000b6d29134feff041f8b17362f4d041b2ebd4f
fcaudillo <fcaudilloperez1971@gmail.com>
2019-05-29 14:35:42 -0400

    Generacion de  script de usuarios de la aplicacion

--- crear_usuarios.sh
+++ crear_usuarios.sh
@@ -1,10 +1,12 @@
-sed -i -e 's/$ADMIN_USER/$1/gI' \
-    -e 's/$ADMIN_PASS/$2/gI' \
-    -e 's/$CAJERO_USER/$3/gI' \
-    -e 's/$CAJERO_PASS/$4/gI' \
-    -e 's/$TAE_USER/$5/gI' \
-    -e 's/$TAE_PASS/$6/gI' \
-    -e 's/$CLIENTE_NOMBRE/$7/gI' \
-    -e 's/$CLIENTE_GIRO/$8/gI' \
-    -e 's/$TICKET_PIE/$9/gI' \
-    -e 's/$ADMIN_USER/$10/gI' crear_usuarios.template > ./scripts/crear_usuarios.py
+
+sed -e "s/ARG_ADMIN_USER/$1/gI" \
+    -e "s/ARG_ADMIN_PASS/$2/gI" \
+    -e "s/ARG_CAJERO_USER/$3/gI" \
+    -e "s/ARG_CAJERO_PASS/$4/gI" \
+    -e "s/ARG_TAE_USER/$5/gI" \
+    -e "s/ARG_TAE_PASS/$6/gI" \
+    -e "s/ARG_CLIENTE_NOMBRE/$7/gI" \
+    -e "s/ARG_CLIENTE_GIRO/$8/gI" \
+    -e "s/ARG_CLIENTE_DIRECCION/$9/gI"  \
+    -e "s/ARG_TICKET_PIE/${10}/gI" crear_usuarios.template > ./scripts/crear_usuarios.py
+ 
---
+++ readme.txt
@@ -0,0 +1,6 @@
+
+Para ejecutar la generacion de usuarios y configuracion.
+xargs --arg-file=usuarios_tlapape.txt  ./crear_usuarios.sh| 
+En donde usuarios_tlapape.txt contiene los usuarios y configuraciones.
+
+
