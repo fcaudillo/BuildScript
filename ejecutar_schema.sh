da521f6af9c81653a2ea5a4ab2a8198883506379
fcaudillo <fcaudilloperez1971@gmail.com>
2019-05-29 13:20:38 -0400

    cambios

--- ejecutar_schema.sh
+++ ejecutar_schema.sh
@@ -1,2 +1,2 @@
-docker run --network=main_default   -it --rm -v ~/data/BuildScript/scripts:/scripts jbergknoff/postgresql-client postgresql://$USUARIO_DB:$PASSWORD_DB@dbtpv:5432/dbtpv -f  /scripts/create_schema_$1.sql
+docker run --network=main_default   -it --rm -v ~/data/BuildScript/scripts:/scripts jbergknoff/postgresql-client postgresql://$USUARIO_ADMIN_DB:$PASSWORD_ADMIN_DB@dbtpv:5432/dbtpv -f  /scripts/create_schema_$1.sql
  
