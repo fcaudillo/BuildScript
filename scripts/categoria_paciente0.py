745dd40226b4e7a000aa6216cfb4ccfdf0ae26bc
Francisco Caudillo <fcaudilloperez1971@gmail.com>
2019-05-30 11:09:29 -0500

    Cambio a  las plantillas

--- scripts/categoria_paciente0.py
+++ scripts/categoria_paciente0.py
@@ -1,15 +1,5 @@
 
-from precios.models import Categoria,TipoMovimiento
-
-TipoMovimiento.objects.create(codigo='INV',description = 'INVENTARIO', factor =1,factor_conta =1, prioridad = 1)
-TipoMovimiento.objects.create(codigo='COM',description = 'COMPRA', factor =1,factor_conta =1, prioridad = 2)
-TipoMovimiento.objects.create(codigo='VTA',description = 'VENTA', factor =-1,factor_conta =-1, prioridad = 3)
-TipoMovimiento.objects.create(codigo='DEV',description = 'DEVOLUCION', factor =1,factor_conta =1, prioridad = 4)
-TipoMovimiento.objects.create(codigo='MER',description = 'MERMA', factor =-1,factor_conta =-1, prioridad = 5)
-TipoMovimiento.objects.create(codigo='PER',description = 'USO PERSONAL', factor =-1,factor_conta =-1, prioridad = 6)
-TipoMovimiento.objects.create(codigo='PRO',description = 'DEVOLUCION PROVEEDOR', factor =-1,factor_conta =-1, prioridad = 7)
-TipoMovimiento.objects.create(codigo='MOD',description = 'MODIFCACION DE PRODUCTOS', factor =0,factor_conta =0, prioridad = 0)
-TipoMovimiento.objects.create(codigo='TAE',description = 'VENTA TIEMPO AIRE', factor =0,factor_conta =1, prioridad = 0)
+from precios.models import Categoria
 
 raiz = Categoria.objects.create(codigo='UNK', description='Raiz')
 categoria1 = Categoria.objects.create(codigo='PAP', description='PAPELERIA', parent = raiz)
