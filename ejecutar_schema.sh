f13dca6cd281b9df825de3410616a29d552d3b99
Francisco Caudillo <fcaudilloperez1971@gmail.com>
2019-05-30 17:08:42 -0500

    Correccion de script

--- ejecutar_schema.sh
+++ ejecutar_schema.sh
@@ -1,2 +1,2 @@
-docker run --network=main_default   -it --rm -v ~/data/BuildScript/scripts:/scripts jbergknoff/postgresql-client postgresql://$USUARIO_ADMIN_DB:$PASSWORD_ADMIN_DB@dbtpv:5432/dbtpv -f  /scripts/create_schema_$1.sql
- 
+docker run --network=main_default   -it --rm -v /home/dockeradm/data/BuildScript:/BuildScript jbergknoff/postgresql-client postgresql://$USUARIO_ADMIN_DB:$PASSWORD_ADMIN_DB@dbtpv:5432/dbtpv -f  /BuildScript/scripts/create_schema_$1.sql
+
