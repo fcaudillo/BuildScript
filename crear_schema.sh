1195e141757774492296ea02037df2bee349a78e
Francisco Caudillo <fcaudilloperez1971@gmail.com>
2019-05-30 17:27:36 -0500

    Modificando rutasde  scripts

--- crear_schema.sh
+++ crear_schema.sh
@@ -1,7 +1,8 @@
-echo "CREATE USER adm_$1 WITH LOGIN NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE;" > ./scripts/create_schema_$1.sql
-echo "CREATE SCHEMA $1 AUTHORIZATION adm_$1;" >> ./scripts/create_schema_$1.sql
-echo "ALTER USER adm_$1 SET search_path = $1, public;" >> ./scripts/create_schema_$1.sql
-echo "ALTER USER adm_$1 WITH PASSWORD '$2';" >> ./scripts/create_schema_$1.sql
-echo "GRANT CONNECT ON DATABASE dbtpv TO adm_$1;" >> ./scripts/create_schema_$1.sql
-echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA $1 TO adm_$1;" >> ./scripts/create_schema_$1.sql
-echo "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA $1 TO adm_$1;" >> ./scripts/create_schema_$1.sql
+mkdir -p /BuildScript/scripts/$1
+echo "CREATE USER adm_$1 WITH LOGIN NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE;" > /BuildScript/scripts/$1/create_schema_$1.sql
+echo "CREATE SCHEMA $1 AUTHORIZATION adm_$1;" >> /BuildScript/scripts/$1/create_schema_$1.sql
+echo "ALTER USER adm_$1 SET search_path = $1, public;" >> /BuildScript/scripts/$1/create_schema_$1.sql
+echo "ALTER USER adm_$1 WITH PASSWORD '$2';" >> /BuildScript/scripts/$1/create_schema_$1.sql
+echo "GRANT CONNECT ON DATABASE dbtpv TO adm_$1;" >> /BuildScript/scripts/$1/create_schema_$1.sql
+echo "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA $1 TO adm_$1;" >> /BuildScript/scripts/$1/create_schema_$1.sql
+echo "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA $1 TO adm_$1;" >> /BuildScript/scripts/$1/create_schema_$1.sql
