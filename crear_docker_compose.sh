95fbc896c1b2e76b6072cc40c99c4abecbee9624
Francisco Caudillo <fcaudilloperez1971@gmail.com>
2019-05-31 17:22:01 -0500

    Correccion de docker-compose

--- crear_docker_compose.sh
+++ crear_docker_compose.sh
@@ -1,2 +1,2 @@
 sed -e "s/ARG_CLIENTE_ID/$1/gI" \
-    -e "s/ARG_PORT/$2/gI" /BuildScript/crear_docker_compose.template > /BuildScript/scripts/$1/main/docker-compose.yml
+    -e "s/ARG_PASSWORD_DB/$2/gI" /BuildScript/crear_docker_compose.template > /BuildScript/scripts/$1/main/docker-compose.yml
